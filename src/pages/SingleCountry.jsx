8c6c86136c2c599b0afd07b715b852326fc71526
elarhadu <elarhadu@gmail.com>
2023-02-10 09:52:57 +0000

    Create page to display the details of a selected country

---
+++ src/pages/SingleCountry.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Country from '../components/Country';
+
+export default function SingleCountry() {
+  return (
+    <>
+      <h2>Hello World</h2>
+      <Country />
+    </>
+  );
+}
