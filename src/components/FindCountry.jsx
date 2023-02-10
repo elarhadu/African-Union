f80652fe98d47d9507de89d17d5c887f0b2c252e
elarhadu <elarhadu@gmail.com>
2023-02-10 14:52:54 +0000

    Add styling for the input field in mobile view

--- src/components/FindCountry.jsx
+++ src/components/FindCountry.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-// import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { searchCountries } from '../redux/countries';
 
@@ -11,15 +10,18 @@ export default function FindCountry() {
     dispatch(searchCountries(name));
   }, [dispatch, name]);
 
+  const inputStyles = {
+    width: '100%',
+    padding: '10px',
+    borderRadius: '25px',
+    fontSize: '16px',
+    backgroundColor: 'white',
+  };
+
   return (
     <section>
       <form className="search" onSubmit={(e) => { e.preventDefault(); }}>
-        <label htmlFor="name">
-          {' '}
-          FIND A COUNTRY
-        </label>
-        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
-        <button type="submit">search</button>
+        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} placeholder="FIND A COUNTRY" style={inputStyles} />
       </form>
     </section>
   );
